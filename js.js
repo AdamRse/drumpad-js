d132948883375817b83da553bb0fbc237ab7fc6f
Garage404 <bakaarion3@gmail.com>
2023-12-14 14:25:09 +0100

    deuxième partie v1

--- js.js
+++ js.js
@@ -1,28 +1,32 @@
 let touches = document.querySelectorAll(".key");
 let audios = document.querySelectorAll("audio");
 
-document.body.addEventListener("keydown", function(e){
+document.addEventListener("keydown", function(e){
     touches.forEach(function(touche){
         if(touche.querySelector("kbd").innerHTML.toLowerCase() == e.key){
             touche.classList.add("playing")
             audios.forEach(function(audio){
                 if(audio.dataset.key == touche.dataset.key){
+                    audio.currentTime = 0;
                     audio.play();
                 }
             })
             
         }
     })
+    if(e.key == "&"){
+        beatBox();
+    }
     
 })
-document.body.addEventListener("keyup", function(e){
+document.addEventListener("keyup", function(e){
     touches.forEach(function(touche){
         if(touche.querySelector("kbd").innerHTML.toLowerCase() == e.key){
             touche.classList.remove("playing")
             audios.forEach(function(audio){
                 if(audio.dataset.key == touche.dataset.key){
-                    audio.pause();
-                    audio.currentTime = 0;
+                    // audio.pause();
+                    // audio.currentTime = 0;
                 }
             })
             
@@ -34,6 +38,7 @@ touches.forEach(function(touche){
         this.classList.add("playing")
         audios.forEach(function(audio){
             if(audio.dataset.key == touche.dataset.key){
+                audio.currentTime = 0;
                 audio.play();
             }
         })
@@ -58,10 +63,49 @@ touches.forEach(function(touche){
         })
     })
 })
+melodie1 = [
+    ["z", 200],
+    ["z", 200],["w", 0],
+    ["z", 200],
+    ["z", 200],["q", 0],
+    ["z", 200],
+    ["z", 200],["w", 0],
+    ["z", 200],
+    ["z", 200],["q", 0],
+    ["z", 200],
+    ["z", 200],["w", 0],
+    ["z", 200],
+    ["z", 200],["q", 0],
+    ["z", 200],
+    ["z", 200],["w", 0],
+    ["z", 200],
+    ["z", 200],["q", 0],
+];
+function beatBox(){
+    simulateKey(melodie1[0][0], 200).then(() => {
+        if(melodie1.length != 0){
+            setTimeout(function(){ beatBox(); }, melodie1[0][1]);
+        }
+
+    });
+}
 
 
+function simulateKey(key, duree = 200){
+    return new Promise((resolve) => {
+        const event = new KeyboardEvent('keydown', { key });
+        document.dispatchEvent(event);
+         console.log("keydown");
+         melodie1.shift();
+        setTimeout(() => {
+            const keyUpEvent = new KeyboardEvent('keyup', { key });
+            document.dispatchEvent(keyUpEvent);
 
-// function playAndStop(key, ){
-    
+            resolve();
+        }, duree);
+    });
+}
 
-// }
+document.querySelector("button").addEventListener("click", function(){
+    beatBox();
+})
